6a98db728073ab4e9bdb2d83d5e9eb6b00acaf1d
Danil Budovskiy <danilbudovskiiy@gmail.com>
2021-10-26 13:54:53 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,36 @@
-var path = require('path');
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader')
+
 module.exports = {
-  //...
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.js'
+    context: path.resolve(__dirname, 'src'),
+    entry: './main.js',
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: true,
+        port: 8080
     }
-  },
-  devServer: {
-    static: path.join(__dirname, 'src'),
-    compress: true,
-    port: 8080
-  }
 };
